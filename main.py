8665862ad87f997044cb2e78e5619b617ed91d41
ReadinGravity <113469951+ReadinGravity@users.noreply.github.com>
2022-09-28 08:50:48 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+mps="ahoj svet"
+print (mps)
+
+#prechadzanie stringu
+for i in range(0,len(mps)): #funckai kt vrati dlzku retzca
+    print (mps[i])
+
+#druhy sposob
+for j in mps:
+    print(j.isupper())
+#v stringu nevies prepisat jednotlive pismenka, premenne su menitelne a nemenitelne
