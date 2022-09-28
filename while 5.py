fa0c921e10aa5d16cc9f3b5c5c159745d0ba440d
ReadinGravity <113469951+ReadinGravity@users.noreply.github.com>
2022-09-28 09:02:32 +0200

    Add files via upload

--- while 5.py
+++ while 5.py
@@ -4,3 +4,12 @@ while slovo[0]!="x":
     cd+=len(slovo)
     slovo=input("povedz mi nejake slovo: ")
 print("pocet znakov: ",cd)
+
+#ako by sa zmenil program aby tam bola zapoc dlzka posledn slova
+
+slovo=input("povedz mi nejake slovo: ")
+cd=len(slovo)
+while slovo[0]!="x":
+    cd+=len(slovo)
+    slovo=input("povedz mi nejake slovo: ")
+print("pocet znakov: ",cd)
