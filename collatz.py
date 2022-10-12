d35385f6ebd0faa0083b637c0e41ed74d3285069
ReadinGravity <113469951+ReadinGravity@users.noreply.github.com>
2022-10-12 09:37:07 +0200

    Add files via upload

--- collatz.py
+++ collatz.py
@@ -1,13 +1,23 @@
-#napis funkciu kt pre zadane cislo vypise jeho cleny colatzovej postupnosti
-def collatz(cislo):
-    while cislo!=1:
-        if cislo %2==0:
-            cislo//=2
-        elif cislo %2==1:
-            cislo=cislo*3+1
-        print(int(cislo), end=" ")
+# #napis funkciu kt pre zadane cislo vypise jeho cleny colatzovej postupnosti
+# def collatz(cislo):
+#     while cislo!=1:
+#         if cislo %2==0:
+#             cislo//=2
+#         elif cislo %2==1:
+#             cislo=cislo*3+1
+#         print(int(cislo), end=" ")
+#
+#
+# collatz(13)
 
 
-collatz(13)
+#napis funckiu kde ked napisem 5, bude z toho pravouhly trojuholnik
+def psc(riadok:int):
+    for cr in range (riadok+1):
+        for k in range(cr+1):
+            print("*"*(cr+1), end=" ")
+        print(" ")
+psc(5)
 
-
+#pekny trojuholni, 1 for na medzery, druhy na *
+#input.sk funkcie 1,2,3
